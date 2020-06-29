78d56fa38ae65fef58a1ad34f0dc0b1b78a6da1e
Dongkyun Lee <dongkyl@amazon.com>
2020-06-29 22:48:58 +0900

    init project

---
+++ infra/bin/infra.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ApiGatewayStack } from '../lib/api-gateway-stack';
+import {
+  Namespace, AppEnv, Region,
+} from '../lib/interfaces/constant';
+import { LambdaStack } from '../lib/lambda-app-stack';
+
+const app = new cdk.App({
+  context: {
+    ns: Namespace,
+    appEnv: AppEnv.DEV,
+    region: Region,
+  },
+});
+
+const lambdaStack = new LambdaStack(app, `LambdaStack${Namespace}`);
+const apiGatewayStack = new ApiGatewayStack(app, `ApiGatewayStack${Namespace}`, {
+  eventHandlerFunction: lambdaStack.eventHandlerFunction,
+});
+apiGatewayStack.addDependency(lambdaStack);
