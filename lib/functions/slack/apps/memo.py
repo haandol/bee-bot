ad1e95639d8ae702909ea9906035d7f3e73b43d0
Dongkyun Lee <dongkyl@amazon.com>
2020-07-14 14:40:04 +0900

    register memo to constant

--- lib/functions/slack/apps/memo.py
+++ lib/functions/slack/apps/memo.py
@@ -13,14 +13,14 @@ def run(robot, channel, user, tokens):
     robot.logger.info(tokens)
 
     msg = '요청하신 작업을 수행할 수 없습니다. 로그를 확인해주세요.'
-    if len(tokens) == 2:
+    if len(tokens) == 1:
         msg = robot.brain.get(get_key(tokens[0]))
         if not msg:
             msg = f'{tokens[0]} 으로 기억해둔 내용을 찾을 수 없습니다.'
-    elif len(tokens) == 3:
+    elif len(tokens) == 2:
         robot.brain.store(get_key(tokens[0]), tokens[1])
         msg = f'{tokens[0]} 를 잘 기억해두었습니다.'
-    elif len(tokens) > 3:
+    elif len(tokens) > 2:
         msg = '저장하실 내용에 띄어쓰기가 들어갈 경우 따옴표(")로 감싸보세요.'
         
     return channel, msg
--- lib/interfaces/constant.ts
+++ lib/interfaces/constant.ts
@@ -2,5 +2,5 @@ export const AccessTokenKey = '__slack_access_token__';    // Should be synced w
 export const VerificationTokenKey = '__slack_verification_token__';    // Should be synced with SSM key
 export const Namespace = 'BeeBot';
 export const Region = 'ap-northeast-2';
-export const Apps = ['hello_world', 'helper'];
+export const Apps = ['hello_world', 'helper', 'memo'];
 export const CmdPrefix = '!';
